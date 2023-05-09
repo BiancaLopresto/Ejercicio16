a235115a4587a79e66b8c4a0778d216142296810
BiancaLopresto <bianlopre19@gmail.com>
2023-05-09 18:58:14 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio16.Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace Ejercicio16.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroingresado;
+
+            Console.Write("Ingrese el Numero: ");
+            numeroingresado = double.Parse(Console.ReadLine());
+
+            if (numeroingresado >= 0)
+            {
+                Console.WriteLine($"El numero {numeroingresado} es Positivo");
+            }
+            else Console.WriteLine($"El numero {numeroingresado} es Negativo");
+
+            
+
+            
+            }
+        }
+    }
